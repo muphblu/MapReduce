89627ab9cd0923d7a4fac2d602e9af197b488e15
Aidar Shaikhiev <muphblu09@gmail.com>
2016-07-08 18:01:24 +0300

    working storage server

--- storage_server.py
+++ storage_server.py
@@ -1,33 +1,44 @@
+import os
 from xmlrpc.server import SimpleXMLRPCServer
 
 
 class StorageServer:
-    def __init__(self):
-        self.server = SimpleXMLRPCServer(("localhost", 8000))
+    def __init__(self, address):
+        """
+        Storage server class
+        should be started in separate thread
+        :param address: tuple with network address and port
+        """
+        self.server = SimpleXMLRPCServer(address)
         self.server.register_function(self.read, "read")
         self.server.register_function(self.write, "write")
         self.server.register_function(self.delete, "delete")
-        # self.server.register_function(self.size, "size")
         self.server.serve_forever()
-        pass
 
     def read(self, chunk_id):
         """
-        Reads chunk from the storage server
+        Reads chunk from the storage server.
+        Raises FileNotFoundError
         :param chunk_id: id of the chunk to be read from storage server
         :return: list of bytes
         """
-        return "return from storage.read()"
-        pass
+        # TODO: put all files in separate directory
+        if not os.path.exists(chunk_id):
+            raise FileNotFoundError()
+        with open(chunk_id, mode='r') as file:
+            return file.read()
 
     def write(self, chunk_id, content):
         """
         Writes chunk to the storage server
+        Raises FileExistsError
         :param chunk_id: id of the chunk to be written
         :param content: content of this chunk represented as list of bytes
         :return: status
         """
-        return "return from storage.read()"
+        with open(chunk_id, mode='x') as file:
+            file.write(content)
+        return True
 
     def delete(self, chunk_id):
         """
@@ -35,7 +46,5 @@ class StorageServer:
         :param chunk_id: id of the chunk to be deleted
         :return: status
         """
-        return "return from storage.read()"
-
-    # def size(self):
-    #     return "return from storage.read()"
+        os.remove(chunk_id)
+        return True
