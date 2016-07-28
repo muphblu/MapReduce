0adf21ba34d4be7a4932549620fabf26f33ce089
Aidar Shaikhiev <muphblu09@gmail.com>
2016-07-28 20:42:40 +0300

    Add Master and JobTracekr stubs

---
+++ job_tracker.py
@@ -0,0 +1,15 @@
+class JobTracker:
+    def __init__(self, options):
+        pass
+
+    def startup(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def start_job(self, data, map_fun, reduce_fun):
+        pass
+
+    def stop_job(self):
+        pass
