aa991d4b8370dafdce15f98597d4fbe785f0bc38
Aidar Shaikhiev <muphblu09@gmail.com>
2016-07-30 15:21:24 +0300

    Doc strings for job_tracker

--- map_reduce/job_tracker.py
+++ map_reduce/job_tracker.py
@@ -4,8 +4,8 @@ import utils
 
 class JobTracker:
     def __init__(self, options):
-        servers_info = utils.get_slaves_info()
-        self.servers = [utils.StorageServerInfo(server[0], server[1]) for server in servers_info]
+        slaves_info = utils.get_slaves_info()
+        self.slaves = [utils.StorageServerInfo(server[0], server[1]) for server in slaves_info]
         config = utils.get_configuration()
         self.mappers_num = config['mappers_num']
         self.reducers_num = config['reducers_num']
@@ -26,18 +26,17 @@ class JobTracker:
     def shutdown(self):
         pass
 
-    def start_job(self, data, map_fun, reduce_fun):
+    def start_job(self, data_path, map_function, reduce_function):
         """
-        :param data: path to the file with data to process by map/reduce
-        :param map_fun: mapper function in str
-        :param reduce_fun: reducer function in str
-        :return:
+        Starts job
+        :param data_path: path to the file with data to process by map/reduce
+        :param map_function: mapper function in str
+        :param reduce_function: reducer function in str
         """
+        self.reduce_fun = reduce_function
         mappers = self._get_mapper_servers()
         self.mappers_status = {mapper.id: False for mapper in mappers}
-        chunks_info = self.naming_server.read(data)
-        # TODO receive chunks list
-        # TODO divide chunks between mappers
+        chunks_info = self.naming_server.read(data_path)
         chunks_count = len(chunks_info)
         # chunks_per_mapper = chunks_count/self.mappers_num
         chunks_for_mappers = [None] * self.mappers_num
@@ -45,23 +44,33 @@ class JobTracker:
             chunks_for_mappers[chunks_count % self.mappers_num].append(chunks_info[i])
 
         for i in range(self.mappers_num):
-            mappers[i].proxy.init_mapper(chunks_for_mappers[i], map_fun)
-
-        self.reduce_fun = reduce_fun
+            mappers[i].proxy.init_mapper(chunks_for_mappers[i], map_function)
 
     def stop_job(self):
         pass
 
     def man_up(self, server_id):
+        """
+        Notification that server is up after being down
+        :param server_id: server that turned on
+        """
         pass
 
     def man_down(self, server_id):
+        """
+        Notification that server is went down
+        :param server_id: server that went down
+        """
         pass
 
     def map_finished(self, server_id):
-        """Notification that map finished on particular server"""
+        """
+        Notification that map finished on particular server
+        Runs reducers when all mappers finished the work
+        :param server_id: mapper that finished the work
+        """
         self.mappers_status[server_id] = True
-        for mapper in self.mappers.items():
+        for mapper in self.mappers_status.items():
             if not mapper[1]:
                 return
         self._start_reducers()
@@ -70,12 +79,21 @@ class JobTracker:
     # Private
     # ==========================================
     def _start_reducers(self):
+        """
+        Starts reducers
+        """
         reducers = self._get_reducer_servers
         for reducer in reducers:
             reducer.proxy.init_reducer(self.mappers_status.keys())
 
     def _get_mapper_servers(self):
-        return self.servers[:self.mappers_num]
+        """
+        :return: List of servers which should be mappers
+        """
+        return self.slaves[:self.mappers_num]
 
     def _get_reducer_servers(self):
-        return self.servers[:self.mappers_num + self.reducers_num]
+        """
+        :return: List of servers which should be reducers
+        """
+        return self.slaves[self.mappers_num:self.mappers_num + self.reducers_num]
